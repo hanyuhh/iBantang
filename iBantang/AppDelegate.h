4ef93c035f2303470aeeca0195cf352321f16a3d
cloudtopxm <cloudtop@cloudtopxmdemacbook-pro.local>
2016-01-18 14:42:54 +0800

    Initial Commit

---
+++ iBantang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iBantang
+//
+//  Created by cloudtopxm on 16/1/18.
+//  Copyright © 2016年 orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iBantang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iBantang
+//
+//  Created by cloudtopxm on 16/1/18.
+//  Copyright © 2016年 orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
